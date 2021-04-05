1795f29d09a12d95c6890a6d66caffee5337125c
BuildTools <is713932@iteso.mx>
2021-04-05 17:37:59 -0500

    Fix: Show text when no snapshots are present

--- commands/log.js
+++ commands/log.js
@@ -4,7 +4,12 @@ module.exports.description = 'Show the log history of snapshots';
 module.exports.command = function (yargs) {
 
     let files = fs.readdirSync('.jpar/snapshots/')
-    for(let i in files){
-        console.log(fs.readJSONSync(`.jpar/snapshots/${files[i]}/snapshot.json`));
+    if (files.length == 0) {
+        console.log('No snapshot history to show');
+    } else {
+
+        for (let i in files) {
+            console.log(fs.readJSONSync(`.jpar/snapshots/${files[i]}/snapshot.json`));
+        }
     }
 }
