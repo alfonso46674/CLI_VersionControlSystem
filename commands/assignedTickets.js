7fabc7d52dbdc47c022918c070b101cc12e42166
BuildTools <is713932@iteso.mx>
2021-05-09 19:12:18 -0500

    Fix: Check if the remote Url is valid

--- commands/assignedTickets.js
+++ commands/assignedTickets.js
@@ -18,28 +18,43 @@ module.exports.command = function (yargs) {
     }).argv;
 
     if (filesFunctions.directoryExists('.jpar')) {
-        
+
         //request the tickets to the remote URL
         let remoteURL = fs.readFileSync('.jpar/refs/remote/server', 'utf8')
+        if (stringIsValidURL(remoteURL)) {
 
-        const options = {
-            method: 'GET',
-            url: remoteURL+`/obtainTicket?devEmail=${argv.email}`
+            const options = {
+                method: 'GET',
+                url: remoteURL + `/obtainTicket?devEmail=${argv.email}`
+            }
+
+            //do the request
+            request(options, function (err, res, body) {
+                if (err || res.statusCode == 400 || res.statusCode == 500) {
+                    console.log(err);
+                    console.error(chalk.red(`Error while retrieving tickets for ${argv.email}`));
+                } else {
+                    console.log((JSON.parse(res.body)))
+                }
+            })
+        } else {
+            console.error(chalk.red('Error at retrieving tickets; Check remote url'));
         }
 
-        //do the request
-        request(options,function(err,res,body){
-            if(err || res.statusCode == 400 || res.statusCode == 500){
-                console.log(err);
-                console.error(chalk.red(`Error while retrieving tickets for ${argv.email}`));
-            }else{
-                console.log((JSON.parse(res.body)))
-            }
-        })
 
 
     } else {
         console.error(chalk.red('.jpar repository not found'));
         process.exit();
+    }
+}
+
+
+function stringIsValidURL(s) {
+    try {
+        new URL(s)
+        return true
+    } catch {
+        return false
     }
 }
